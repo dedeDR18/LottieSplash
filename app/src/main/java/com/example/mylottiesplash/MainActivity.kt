827c3e5084a304eeacf8682071a223daaf990eb9
dededarirahmadi <dededarirahmadi@gmail.com>
2021-03-09 01:45:01 +0700

    second commit

--- app/src/main/java/com/example/mylottiesplash/MainActivity.kt
+++ app/src/main/java/com/example/mylottiesplash/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.mylottiesplash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this, "berhasil", Toast.LENGTH_SHORT).show()
     }
 }
